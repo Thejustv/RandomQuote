e800ffbf6c64c04e2f8f949791401d5c85089d16
Thejus T V <thejustv22@gmail.com>
2023-12-09 22:52:46 +0530

    optimization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
-import { useState,useEffect,useRef } from 'react';
+import { useState,useEffect,useRef,useCallback,useMemo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
 import {faQuoteLeft} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
-  let colors = ['#16a085','#27ae60','#2c3e50','#f39c12','#e74c3c','#9b59b6','#FB6964','#342224','#472E32','#BDBB99','#77B1A9','#73A857'];
+  let colors = useMemo(()=>['#16a085','#27ae60','#2c3e50','#f39c12','#e74c3c','#9b59b6','#FB6964','#342224','#472E32','#BDBB99','#77B1A9','#73A857'],[]);
   const isMounted=useRef(false);
-  let [quote,setQuote]=useState('');
-  let [author,setAuthor]=useState('');
+  let [quote,setQuote]=useState([]);
 
-  async function fetchData(){
+  let changeColorToRandom = useCallback(() => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor=colors[randomIndex];
+    document.documentElement.style.setProperty('--color', randomColor);
+  },[colors]);
+
+  const fetchData=useCallback(async()=>{
     const respone=await fetch("https://api.quotable.io/quotes/random");
     const data=await respone.json();
-    // eslint-disable-next-line
-    data.map((i)=>{return setQuote(i.content),setAuthor(i.author);
-    });
+;   setQuote(data[0]);
     changeColorToRandom();
-    }
+    },[changeColorToRandom]);
 
   useEffect(()=>{
     if(!isMounted.current)
@@ -26,19 +29,12 @@ function App() {
     fetchData();
     isMounted.current=true;
     }
-     // eslint-disable-next-line
-  },[]);
-
-  let changeColorToRandom = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const randomColor=colors[randomIndex];
-    document.documentElement.style.setProperty('--color', randomColor);
-  };
+  },[fetchData]);
 
   return (
       <div id="quote-box">
-        <div id="text"><FontAwesomeIcon icon={faQuoteLeft} size="lg"/> {quote} </div>
-        <div id="author">- {author}</div>
+        <div id="text"><FontAwesomeIcon icon={faQuoteLeft} size="lg"/> {quote.content} </div>
+        <div id="author">- {quote.author}</div>
         <div id="bottom">
           <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_top"><FontAwesomeIcon icon={faSquareTwitter} size="2xl"/></a>
           <button id="new-quote" onClick={fetchData}>New Quote</button>
